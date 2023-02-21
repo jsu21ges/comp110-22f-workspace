ba3e2921b61ac8ba50d8dd35173e290919210fae
nitsuj <nitsuj@ad.unc.edu>
2023-02-21 15:36:54 -0500

    assn1

---
+++ test/array.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class array {
+    String _fName;
+    String _lName;
+
+    static U
+    public static void main(main(String[] args)){
+        Scanner s = new Scanner(System.in);
+
+    }
+}
---
+++ test/src/and_or.java
@@ -0,0 +1,5 @@
+public class and_or {
+    public static void main(String[] args) {
+        System.out.println(true||false);
+    }
+}
